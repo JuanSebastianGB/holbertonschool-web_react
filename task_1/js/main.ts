7c0f605370c5fe29d0861a8179a0b8ddadd0ac78
JuanGonzaGB <3393@holbertonstudents.com>
2022-09-13 02:13:15 +0000

    3. Printing teachers

--- task_1/js/main.ts
+++ task_1/js/main.ts
@@ -29,3 +29,12 @@ const director1: Directors = {
   numberOfReports: 17,
 };
 console.log(director1);
+
+interface TeacherToPrint{
+  firstName: string;
+  lastName: string;
+}
+
+const printTeacher = (teacher: TeacherToPrint): string => {
+  `${firstName[0]. ${lastName}}`;
+};
