6c3b70322ca68c9d1b2e69145b5aca0eb32c2665
JuanGonzaGB <3393@holbertonstudents.com>
2022-09-14 02:54:16 +0000

    8. Ambient Namespaces

--- task_2/js/main.js
+++ task_2/js/main.js
@@ -1,23 +1,35 @@
 /* The Director class implements the DirectorInterface and defines the workFromHome, getCoffeeBreak,
 and workDirectorTasks methods. */
 var Director = /** @class */ (function () {
-    function Director() {
-        this.workFromHome = function () { return 'Working from home'; };
-        this.getCoffeeBreak = function () { return 'Getting a coffee break'; };
-        this.workDirectorTasks = function () { return 'Getting to director tasks'; };
-    }
-    return Director;
-}());
+  function Director() {
+    this.workFromHome = function () {
+      return 'Working from home';
+    };
+    this.getCoffeeBreak = function () {
+      return 'Getting a coffee break';
+    };
+    this.workDirectorTasks = function () {
+      return 'Getting to director tasks';
+    };
+  }
+  return Director;
+})();
 /* The Teacher class implements the TeacherInterface, which means that it must implement all the
 methods that are defined in the interface */
 var Teacher = /** @class */ (function () {
-    function Teacher() {
-        this.workFromHome = function () { return 'cannot work from Some'; };
-        this.getCoffeeBreak = function () { return 'Cannot have a break'; };
-        this.workTeacherTasks = function () { return 'Getting to work'; };
-    }
-    return Teacher;
-}());
+  function Teacher() {
+    this.workFromHome = function () {
+      return 'cannot work from Some';
+    };
+    this.getCoffeeBreak = function () {
+      return 'Cannot have a break';
+    };
+    this.workTeacherTasks = function () {
+      return 'Getting to work';
+    };
+  }
+  return Teacher;
+})();
 var newObject = new Teacher();
 /**
  * If the salary is a number and less than 500, return a Teacher, otherwise return a Director.
@@ -25,9 +37,8 @@ var newObject = new Teacher();
  * @returns Director | Teacher
  */
 var createEmployee = function (salary) {
-    if (typeof salary === 'number' && salary < 500)
-        return new Teacher();
-    return new Director();
+  if (typeof salary === 'number' && salary < 500) return new Teacher();
+  return new Director();
 };
 /**
  * If the employee is a Director, return true, otherwise return false.
@@ -35,19 +46,18 @@ var createEmployee = function (salary) {
  * @returns A boolean
  */
 var isDirector = function (employee) {
-    return employee instanceof Director;
+  return employee instanceof Director;
 };
 /**
  * If the employee is a Director, then call workDirectorTasks, otherwise call workTeacherTasks.
  * @param {DirectorInterface | TeacherInterface} employee - DirectorInterface | TeacherInterface
  */
 var executeWork = function (employee) {
-    if (employee instanceof Director) {
-        return employee.workDirectorTasks();
-    }
-    else if (employee instanceof Teacher) {
-        return employee.workTeacherTasks();
-    }
+  if (employee instanceof Director) {
+    return employee.workDirectorTasks();
+  } else if (employee instanceof Teacher) {
+    return employee.workTeacherTasks();
+  }
 };
 executeWork(createEmployee(200));
 executeWork(createEmployee(1000));
@@ -57,9 +67,7 @@ executeWork(createEmployee(1000));
  * @returns A string
  */
 var teachClass = function (todayClass) {
-    if (todayClass === 'Math')
-        return 'Teaching Math';
-    else if (todayClass === 'History')
-        return 'Teaching History';
+  if (todayClass === 'Math') return 'Teaching Math';
+  else if (todayClass === 'History') return 'Teaching History';
 };
 teachClass('Math');
--- task_3/js/main.ts
+++ task_3/js/main.ts
@@ -1,8 +1,9 @@
 /// <reference path="crud.d.ts" />
+
 import { RowElement, RowID } from './interface';
 import * as CRUD from './crud';
 
-const row: RowElement = { firstName: 'Guillaume', lastName: 'Salva' };
+const row: <RowElement> = { firstName: 'Guillaume', lastName: 'Salva' };
 
 const newRowID: RowID = CRUD.insertRow(row);
 
