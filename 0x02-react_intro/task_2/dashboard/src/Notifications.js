b8467e59a0460326cd92426c7b22e56a2ae81e83
JuanGonzaGB <3393@holbertonstudents.com>
2022-09-20 12:43:56 +0000

    Progressing in task 3

--- 0x02-react_intro/task_2/dashboard/src/Notifications.js
+++ 0x02-react_intro/task_2/dashboard/src/Notifications.js
@@ -1,8 +1,29 @@
 import React from 'react';
 import './Notifications.css';
+import close from './close.png';
+
+const buttonStyles = {
+  display: 'inline',
+  position: 'absolute',
+  right: '0.3rem',
+  top: '0.3rem',
+  background: 'none',
+  border: 'none',
+  width: '1rem',
+  height: '1rem',
+};
+
+const handleClick = () => console.log('Close button has been clicked');
 
 const Notifications = () => {
-  return <div className="Notifications">Here is the list of notifications</div>;
+  return (
+    <div className="Notifications">
+      Here is the list of notifications
+      <button onClick={handleClick} style={buttonStyles} aria-label="close">
+        <img src={close} alt="close" />
+      </button>
+    </div>
+  );
 };
 
 export default Notifications;
