561bee5531970b0b6d4562e2cba5a0699c376a61
JuanGonzaGB <3393@holbertonstudents.com>
2022-09-21 01:11:19 +0000

    Updating test for utils

--- 0x02-react_intro/task_3/dashboard/src/utils.test.js
+++ 0x02-react_intro/task_3/dashboard/src/utils.test.js
@@ -1,20 +1,18 @@
 import { getFullYear, getFooterCopy, getLatestNotification } from './utils';
 
-test('Use fullYear function must return the actual year', () => {
-  const actualDate = new Date().getFullYear();
-  expect(actualDate).toBe(getFullYear());
-});
-
-test('Returns Holberton School if isIndex is true', () => {
-  expect(getFooterCopy(true)).toBe('Holberton School');
-});
-
-test('Returns Holberton School main dashboard if isIndex is false', () => {
-  expect(getFooterCopy(false)).toBe('Holberton School main dashboard');
-});
-
-test('Must return <strong>Urgent requirement</strong> - complete by EOD', () => {
-  expect(getLatestNotification()).toBe(
-    '<strong>Urgent requirement</strong> - complete by EOD'
-  );
+describe('utils', () => {
+  test('getFullYear returns current year', () => {
+    expect(getFullYear()).toEqual(new Date().getFullYear());
+  });
+  test('getFooterCopy returns correct string when input is true', () => {
+    expect(getFooterCopy(true)).toEqual('Holberton School');
+  });
+  test('getFooterCopy returns correct string when input is false', () => {
+    expect(getFooterCopy(false)).toEqual('Holberton School main dashboard');
+  });
+  test('getLatestNotification returns correct string', () => {
+    expect(getLatestNotification()).toEqual(
+      '<strong>Urgent requirement</strong> - complete by EOD'
+    );
+  });
 });
