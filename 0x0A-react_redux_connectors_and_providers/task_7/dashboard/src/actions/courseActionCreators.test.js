bed0311550fa69df36bcb4b6e8af68d38dd90f7b
Juan Sebastian Gonzalez <3393@holbertonstudents.com>
2022-11-13 21:06:45 -0500

    24. Connect courses: create a fetch courses function

--- 0x0A-react_redux_connectors_and_providers/task_7/dashboard/src/actions/courseActionCreators.test.js
+++ 0x0A-react_redux_connectors_and_providers/task_7/dashboard/src/actions/courseActionCreators.test.js
@@ -1,4 +1,8 @@
-import { selectCourse, unSelectCourse } from '../../courseActionCreators';
+import {
+  fetchCourses,
+  selectCourse,
+  unSelectCourse,
+} from './courseActionCreators';
 import { SELECT_COURSE, UNSELECT_COURSE } from './courseActionTypes';
 
 describe('actions course', () => {
@@ -7,5 +11,25 @@ describe('actions course', () => {
   });
   it('Calling the creator unSelectCourse with 1 as argument should return: { type: UNSELECT_COURSE, index: 1 }', () => {
     expect(unSelectCourse(1)).toEqual({ type: UNSELECT_COURSE, index: 1 });
+  });
+  it('Calling the creator fetchCourses should return a data with the list of courses', () => {
+    const courseList = [
+      {
+        id: 1,
+        name: 'ES6',
+        credit: 60,
+      },
+      {
+        id: 2,
+        name: 'Webpack',
+        credit: 20,
+      },
+      {
+        id: 3,
+        name: 'React',
+        credit: 40,
+      },
+    ];
+    expect(fetchCourses().payload).toEqual(courseList);
   });
 });
